7831127d1e2aed47524b1a409be65b7683ecd13a
Francis Wallinger <francis.wallinger@me.com>
2020-07-03 12:02:53 +0000

    Removed sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@ CACHE=/mnt/host/var/app_data
 #
 # Create and Pre-fill cache by copying (slowly) to a temp location and then quickly switching the /mnt/host/var/app_data
 # ** there ciould be another copy of this running (in error) and we don't want to trigger that to empty cacheable **
-sudo [ -d ${CACHE}+ ] && sudo rm -rf ${CACHE}+
-sudo cp -rp ${SRC} ${CACHE}+
+[ -d ${CACHE}+ ] && rm -rf ${CACHE}+
+cp -rp ${SRC} ${CACHE}+
 
-sudo [ -d ${CACHE}- ] && sudo rm -rf ${CACHE}-
-sudo [ -d ${CACHE} ] && sudo mv ${CACHE}-
-sudo mv ${CACHE}+ ${CACHE}
-sudo [ -d ${CACHE}- ] && sudo rm -rf ${CACHE}-
+[ -d ${CACHE}- ] && rm -rf ${CACHE}-
+[ -d ${CACHE} ] && mv ${CACHE}-
+mv ${CACHE}+ ${CACHE}
+[ -d ${CACHE}- ] && rm -rf ${CACHE}-
 
 # Provide a port for testing (inter-node sync TBD)
 unison  -socket 9001  &
