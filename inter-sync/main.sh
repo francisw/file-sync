56f1c1719a72f749d13c4ea7e08e7112df308b81
Francis Wallinger <francis.wallinger@me.com>
2020-07-06 08:40:25 +0000

    checkin

--- inter-sync/main.sh
+++ inter-sync/main.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-SRC=/mnt/app_data/cacheable
-CACHE=/mnt/host/var/app_data
-[ ! -d ${SRC}/.file-sync ] && mkdir ${SRC}/.file-sync && chmod a+rwx ${SRC}/.file-sync
-cd ${SRC}/.file-sync
+cd app_data/.file-sync
 
-#
-# Create and Pre-fill cache by copying (slowly) to a temp location and then quickly switching the /mnt/host/var/app_data
-# ** there ciould be another copy of this running (in error) and we don't want to trigger that to empty cacheable **
-[ -d ${CACHE}+ ] && rm -rf ${CACHE}+
-cp -rp ${SRC} ${CACHE}+
+AVAILABLE=`ls advertised`
 
-[ -d ${CACHE}- ] && rm -rf ${CACHE}-
-[ -d ${CACHE} ] && mv ${CACHE} ${CACHE}-
-mv ${CACHE}+ ${CACHE}
-[ -d ${CACHE}- ] && rm -rf ${CACHE}-
+sleep 600
 
-# Sync this nodes SRC and CACHE
-unison 	-root ${SRC} \
-	-root ${CACHE} \
-	-repeat watch \
-	-prefer newer  
+exit 0
