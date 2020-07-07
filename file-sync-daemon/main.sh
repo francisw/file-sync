62422241c2527b7c77a0218804a274cfb4d40efb
Francis Wallinger <francis.wallinger@me.com>
2020-07-07 22:11:39 +0000

    Share

--- file-sync-daemon/main.sh
+++ file-sync-daemon/main.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-[ -z $NODENAME ] && echo "NODENAME env required" && exit 1
-[ -z $ADVERTISE_PORT ] && echo "ADVERTISE_PORT env required" && exit 1
-[ ! -d $NODENAME ] && mkdir $NODENAME
-echo > $NODENAME/$ADVERTISE_PORT
+SHARE=/mnt/app_data/cacheable/.file-sync
+
+[ -z ${NODENAME} ] && echo "NODENAME env required" && exit 1
+[ -z ${ADVERTISE_PORT} ] && echo "ADVERTISE_PORT env required" && exit 1
+[ ! -d ${SHARE}/${NODENAME} ] && mkdir ${SHARE}/${NODENAME}
+echo > ${SHARE}/${NODENAME}/${ADVERTISE_PORT}
 
 exec unison -socket 9999
