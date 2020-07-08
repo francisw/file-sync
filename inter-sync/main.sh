f61c61bad781936bccee888e3549b4a7ff18b7e5
Francis Wallinger <francis.wallinger@me.com>
2020-07-08 13:36:57 +0000

    checkin

--- inter-sync/main.sh
+++ inter-sync/main.sh
@@ -5,11 +5,15 @@ CACHE=/mnt/host/var/app_data
 
 cd /mnt/app_data/cacheable/.file-sync
 
-# Spec is {node}/{port}
-# Cleanup old
+# Advert spec is {node}/{port}
+# Idea for a Cleanup old if healtcheck refreshes the advert_taken files
 # find * -type f -mmin +5 -exec rm -f {} +
 
 [ -z $NODENAME ] && echo "NODENAME env required" && exit 1
+
+# We don't want adverts from this node, or nodes that this node is bound to already
+# Each other advert taken by this node is stored in {ThisNode}/.{ConnectedNode}
+IGNORE=(`ls -d ${NODENAME}/.*`)
 
 # Get all adverts not from this node
 ADVERTS=(`ls */* | grep -v $NODENAME`)
@@ -18,10 +22,15 @@ if [ ${#ADVERTS[@]} -eq 0 ]; then
 	exit 0
 fi
 
-# Remove the advert and reformat from nodname/portnumber to nodename:portnumber
-rm ${ADVERTS[0]}
-TARGET=${ADVERTS[0]//[\/]/:}
-echo ${NODENAME} >> .${TARGET}  # so we have some record of who is synching with whom
+# Remove the advert so nobody else takes it, and reformat from nodename/portnumber to nodename:portnumber
+rm ${ADVERTS[0]}				# ${ADVERTS[0]} = "AdvertisedNode/AdvertisedPort"
+IFS='/' read -ra ADVERT <<< "${ADVERTS[0]}" 	# $ADVERT = Array( AdvertisedNode, AdvertisedPort )
+ADVERTISED_NODE=${ADVERT[0]}
+ADVERTISED_PORT=${ADVERT[1]}
+
+TARGET="${ADVERTISED_NODE}:${ADVERTISED_PORT}"
+ADVERT_TAKEN="${NODENAME}/.${ADVERTISED_NODE}"  # Filename to announce what we are doing, so we have some record of who is synching with whom
+echo ${ADVERTISED_PORT} > ${ADVERT_TAKEN}  	# so we have some record of who is synching with whom
 echo "${NODENAME} taking advertised ${TARGET}" >&2
 
 echo unison 	\
@@ -38,5 +47,5 @@ unison 	\
 	-prefer newer  
 
 EXITCODE=$?
-rm -f .${TARGET}
+rm -f ${ADVERT_TAKEN}
 exit $EXITCODE
